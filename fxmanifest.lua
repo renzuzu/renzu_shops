428f8457462a0e5007582d764945c8b9b266e683
renzuzu <82306584+renzuzu@users.noreply.github.com>
2022-11-22 04:24:08 +0800

    chore: version 1.102

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,12 @@
 fx_version 'cerulean'
 lua54 'yes'
 game 'gta5'
+ 
+author       'renzuzu'
+version      '1.102'
+license      'LGPL-3.0-or-later'
+repository   'https://github.com/renzuzu/renzu_shops'
+description  'Advanced Shops available for free.'
 
 shared_scripts {
 	'@ox_lib/init.lua',
